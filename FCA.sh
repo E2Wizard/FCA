0aac881510c5d5da472192e1565c8905ea399a36
E2Wizard <martinradovanovicnenad@gmail.com>
2024-08-06 18:53:35 +0200

    Update FCA.sh

--- FCA.sh
+++ FCA.sh
@@ -238,7 +238,7 @@ password=$(generate_random_letters 2>/dev/null)
 echo "Username: $username" >/dev/null 2>&1
 echo "Password: $password" >/dev/null 2>&1
 
-curl -k -A -k -s 'http://www.clinehub.com/freetest/' --compressed -X POST \
+curl --max-time 5.5  -k -A -k -s 'http://www.clinehub.com/freetest/' --compressed -X POST \
   -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0' \
   -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8' \
   -H 'Accept-Language: cs,sk;q=0.8,en-US;q=0.5,en;q=0.3' \
